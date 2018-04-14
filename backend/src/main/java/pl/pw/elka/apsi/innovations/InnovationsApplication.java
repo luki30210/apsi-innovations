985aa39670f38f3bd7587be0e70b8408ef37c9e9
Łukasz <lukasz30210@gmail.com>
2018-04-14 23:06:54 +0200

    initial spring boot web app

---
+++ backend/src/main/java/pl/pw/elka/apsi/innovations/InnovationsApplication.java
@@ -0,0 +1,12 @@
+package pl.pw.elka.apsi.innovations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnovationsApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/pl/pw/elka/apsi/innovations/InnovationsApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.pw.elka.apsi.innovations;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InnovationsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
